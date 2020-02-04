5213509eccee449c980e2251c69ff6ec76adbf46
Ubuntu <ubuntu@ip-10-10-1-73.ap-northeast-1.compute.internal>
2020-02-04 06:18:17 +0000

    second commit

--- main.tf
+++ main.tf
@@ -1,3 +1,77 @@
 terraform {
-	required_version = "~> 0.12"
+  required_version = "~> 0.12" 
+}
+
+provider "azurerm" {
+  client_id = var.client_id
+  tenant_id = var.tenant_id
+  subscription_id = var.subscription_id
+  client_secret = var.client_secret
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "yuri-vm-${count.index}"
+  count = var.hello_tf_instance_count
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.*.id[count.index]]
+  vm_size               = var.vm_size
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "vmadmin"
+    admin_password = var.admin_password
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "yuri-osdisk-${count.index}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "yuri-group"
+  location = var.location
+}
+
+
+resource "azurerm_virtual_network" "example" {
+  name                = "yuri-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name   = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "yuri-subnet"
+  resource_group_name   = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "yuri-nw-interface-${count.index}"
+  count = var.hello_tf_instance_count
+  location            = var.location
+  resource_group_name   = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "yuri-ip-config"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    environment = "payground"
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "vm_size" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
+variable "location" {}
+variable "admin_password" {}
+variable "hello_tf_instance_count" {
+    default = 1
+}
